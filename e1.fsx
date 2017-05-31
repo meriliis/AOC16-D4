0417ae2fbf4d220192871bef1319fde24421af08
Meri Liis Treimann <meri.treimann@ttu.ee>
2017-05-31 11:42:53 +0300

    solutions

---
+++ e1.fsx
@@ -0,0 +1,73 @@
+﻿open System
+open System.Text.RegularExpressions
+
+let location = "C:/Users/Meri/Documents/advent-of-code/D4"
+let input = System.IO.File.ReadAllLines(location + "/input.txt") |> Array.toList
+
+let getLetters (room : string) : string = 
+    Regex.Match(room, "\D+").Value
+
+let getID (room : string) : string =
+    Regex.Match(room, "\d+").Value
+
+let getChecksum (room : string) : string =
+    Regex.Match(room, "\[(\S+)\]").Groups.[1].Value
+
+let rec splitToParts (list : string list) : string list list = 
+    match list with
+    | []     -> [] 
+    | h :: t -> [getLetters h; getID h; getChecksum h] :: splitToParts t
+
+let getCharacters (letters : string) : string =
+    letters.Replace("-", "")
+
+let topFive (characters : string) = 
+    let rec addToMap (characters : string) (map : Map<string, int>) = 
+        match characters with
+        | s     -> let letter = s.Substring(0, 1)   
+                   if map.ContainsKey (letter) then 
+                        let newMap = map.Add(letter, map.[letter] + 1) 
+                        if s.Length = 1 then newMap else addToMap (s.Substring(1)) newMap
+                   else 
+                        let newMap = map.Add(letter, 1)
+                        if s.Length = 1 then newMap else addToMap (s.Substring(1)) newMap
+
+    let map = addToMap characters Map.empty<string, int>
+
+    let sortFun (pair1 : (string * int)) (pair2 : (string * int))=
+        if (snd pair1 > snd pair2) then
+            -1
+        else if (snd pair1 < snd pair2) then
+            1
+        else
+            String.Compare(fst pair1, fst pair2)
+
+
+
+    let rec takeLetters (list : (string * int) list) (acc : string) =
+        match list with
+        | []     -> acc
+        | h :: t -> takeLetters t (acc + (fst h))
+        
+    let list = map 
+               |> Map.toList 
+               |> List.sortWith sortFun 
+               |> List.take 5
+    
+    takeLetters list ""
+
+let realRooms (rooms : string list list) = 
+    rooms |> List.filter (fun room -> 
+        let actualTop = room.[0] |> getCharacters |> topFive
+        let givenTop = room.[2]
+        actualTop = givenTop
+        )
+
+let rooms = input |> splitToParts
+
+let realOnes = rooms |> realRooms
+
+let rec sumOfIDs (realOnes : string list list) =
+    match realOnes with
+    | []     -> 0
+    | h :: t -> Int32.Parse(h.[1]) + sumOfIDs t
---
+++ e2.fsx
@@ -0,0 +1,61 @@
+﻿open System
+open System.Text.RegularExpressions
+
+let location = "C:/Users/Meri/Documents/advent-of-code/D4"
+let input = System.IO.File.ReadAllLines(location + "/input.txt") |> Array.toList
+
+let getLetters (room : string) : string = 
+    Regex.Match(room, "\D+").Value
+
+let getID (room : string) : string =
+    Regex.Match(room, "\d+").Value
+
+let getChecksum (room : string) : string =
+    Regex.Match(room, "\[(\S+)\]").Groups.[1].Value
+
+let rec splitToParts (list : string list) : string list list = 
+    match list with
+    | []     -> [] 
+    | h :: t -> let words = getLetters h
+                let id = getID h 
+                let checksum = getChecksum h
+                [words; id; checksum] :: splitToParts t
+
+let replaceDash (letters : string) : string =
+    letters.Replace("-", " ")
+
+let abc = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m";"n";"o";"p";"q";"r";"s";"t";"u";"v";"w";"x";"y";"z"]
+
+let rec findLetter (current : string) (id : string) = 
+    let noOfLetters = abc.Length
+    let shift = Int32.Parse(id) % noOfLetters
+    let currentIndex = List.findIndex (fun letter -> letter = current) abc
+
+    if shift > (noOfLetters - currentIndex - 1) then 
+        abc.[shift - (noOfLetters - currentIndex - 1) - 1]
+    else 
+        abc.[currentIndex + shift]
+
+let rec replaceLetters (room : string list) =
+    let words = room.[0]
+    let id = room.[1]
+    let rec helper (words : string) acc = 
+        match words with
+        | s -> let oldLetter = s.Substring(0, 1)
+               let newLetter = 
+                    if oldLetter = "-" then " "
+                    else findLetter oldLetter id 
+               if s.Length = 1 then 
+                    acc + newLetter 
+               else 
+                    helper (s.Substring(1)) (acc + newLetter)
+
+    let actualWords = helper words ""
+    [actualWords; id; room.[2]]
+            
+let replaced = input 
+               |> splitToParts 
+               |> List.map replaceLetters
+
+let northPoleObjects = replaced
+                       |> List.filter (fun room -> room.[0].Contains("north"))
